10c611648b3afab277bda28f0dd758c0c08d4762
TrandyAndy <43793912+TrandyAndy@users.noreply.github.com>
2020-11-27 13:00:21 +0100

    Struktur und Vorbereitung II

---
+++ src/main.cpp
@@ -0,0 +1,19 @@
+
+
+
+//#include "Global.h"
+#include "Arduino.h"
+
+
+void setup() {
+
+  Serial.begin(115200);
+  Serial.println("Setup Abgeschlossen !");
+
+}
+
+
+void loop() { //Looplooplooplooplooplooplooplooplooplooplooplooplooplooplooplooplooplooplooplooplooplooplooplooplooplooplooplupi
+  Serial.println("Hallo Team");
+  delay(1000);
+} // Loop Endeendeendeendeendeendeendeendeendeendeendeendeendeendeendeendeendeendeendeendeendeendeendeendeende
