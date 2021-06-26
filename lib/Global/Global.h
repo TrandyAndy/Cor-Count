1b62b975e7d4a5f378f97235357a9adf1ffaa1e1
JLS666 <43670180+JLS666@users.noreply.github.com>
2021-06-26 17:34:52 +0200

    Neue Pins

--- lib/Global/Global.h
+++ lib/Global/Global.h
@@ -25,8 +25,8 @@ Autor: Andy
 #define pinCamereaEventEntry 33 
 #define pinCameraEventExit 25
 #define pinCameraWakeUp 26
-#define SDA 23 
-#define SCL 22          //SDA pin 23 and SCL pin 19 work fine also using lolin 32 lite.
+#define SDA 16                  (23)
+#define SCL 18          //SDA pin 23 and SCL pin 19 work fine also using lolin 32 lite. (18)
 
 
 
